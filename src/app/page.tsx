84fffc7ee91c69bab6eaf607705441322f4960b3
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-08-08 22:56:29 +0300

    Add Company Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import StatusLabel, { Status } from '@/app/components/status-label';
+import { headers } from 'next/headers';
+import AddCompanyButton from '@/app/components/add-company-button';
+import dynamic from 'next/dynamic';
+
+const Clock = dynamic(() => import('@/app/components/clock'), { ssr: false });
 
 export default function Home() {
+  const headerList = headers();
+
+  console.log(headerList);
+
   return (
     <main>
+      <Clock />
+      <AddCompanyButton />
+
       <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
