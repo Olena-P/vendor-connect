29d3c4dda27fb5ac4194b6be04d61c50468b6050
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-08-16 11:01:59 +0300

    Add Intercepting Routes for companies

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,30 +1,13 @@
 'use client';
 
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 import Button from '@/app/components/button';
-import { CompanyFormModalProps } from './company-form-modal';
-
-const CompanyFormModal = dynamic(
-  () => import('./company-form-modal').then((mod) => mod.CompanyFormModal),
-  {
-    ssr: false,
-  }
-) as React.ComponentType<CompanyFormModalProps>;
 
 export default function AddCompanyButton() {
-  const [show, setShow] = useState(false);
+  const router = useRouter();
 
   return (
-    <>
-      <Button onClick={() => setShow(true)}>Add company</Button>
-      {show && (
-        <CompanyFormModal
-          onSubmit={console.log}
-          show={show}
-          onClose={() => setShow(false)}
-        />
-      )}
-    </>
+    <Button onClick={() => router.push('/companies/new')}>Add company</Button>
   );
 }
