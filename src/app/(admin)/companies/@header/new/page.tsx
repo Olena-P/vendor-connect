cb9edb1d92ee2ce87c7bb0587b2d33e177da7c09
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-08-16 11:01:59 +0300

    Add Intercepting Routes for companies

--- src/app/(admin)/companies/@header/new/page.tsx
+++ src/app/(admin)/companies/@header/new/page.tsx
@@ -1,9 +1,8 @@
-'use client';
-
 import React from 'react';
+import Header from '@/app/components/header';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return <div className={}></div>;
+  return <Header>Add new company</Header>;
 }
