c76758e89be7158c9cca9579bb8589d80bd3ffc7
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-08-22 08:11:26 +0300

    refactor: update Home component to include Sidebar and Header, remove dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
-import { headers } from 'next/headers';
-import AddCompanyButton from '@/app/components/add-company-button';
-import dynamic from 'next/dynamic';
-import MagicButton from '@/app/components/magic-button';
-
-const Clock = dynamic(() => import('@/app/components/clock'), { ssr: false });
+import Header from '@/app/components/header';
+import React from 'react';
+import Sidebar from '@/app/components/sidebar';
 
 export default function Home() {
-  const headerList = headers();
-
-  console.log(headerList);
-
   return (
     <main>
-      <Clock />
-      <AddCompanyButton />
-
-      <MagicButton />
+      <Sidebar />
+      <div className="ml-60">
+        <Header>Welcome</Header>
+      </div>
     </main>
   );
 }
